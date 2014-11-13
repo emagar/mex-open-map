721b03245e2675e4f7e80e0c939cc66ce94390a1
emagar <emagar@gmail.com>
2014-11-13 10:35:50 -0600

    Version sent to Houston conference

---
+++ presentations/auto/uhBias2014.el
@@ -0,0 +1,26 @@
+(TeX-add-style-hook "uhBias2014"
+ (lambda ()
+    (TeX-run-style-hooks
+     "multirow"
+     "tabulary"
+     "tikz"
+     "color"
+     "graphicx"
+     "amsmath"
+     "babel"
+     "es-nodecimaldot"
+     "spanish"
+     "inputenc"
+     "ansinew"
+     "fontenc"
+     "T1"
+     "ae"
+     "url"
+     "beamerthemesplit"
+     "pifont"
+     ""
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "xcolor=dvipsnames")))
+
