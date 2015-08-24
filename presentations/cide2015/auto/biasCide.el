9e54c6660c72847df8bd2d9cb906181259895d55
emagar <emagar@gmail.com>
2015-08-24 09:57:02 -0500

    EM added presentation for CIDE

---
+++ presentations/cide2015/auto/biasCide.el
@@ -0,0 +1,30 @@
+(TeX-add-style-hook "biasCide"
+ (lambda ()
+    (LaTeX-add-labels
+     "fr:MxMapProj")
+    (TeX-add-symbols
+     "mc")
+    (TeX-run-style-hooks
+     "arydshln"
+     "multirow"
+     "tabulary"
+     "tikz"
+     "color"
+     "graphicx"
+     "amsmath"
+     "babel"
+     "es-nodecimaldot"
+     "spanish"
+     "inputenc"
+     "ansinew"
+     "fontenc"
+     "T1"
+     "ae"
+     "url"
+     "beamerthemesplit"
+     ""
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "xcolor=dvipsnames")))
+
