f27d4af40e6d100c3524b67616e8f73b94da115c
emagar <emagar@gmail.com>
2015-10-29 22:22:41 -0600

    EM added ogp and colmex presentations

---
+++ presentations/colmex2015/auto/colmexStrategy2015.el
@@ -0,0 +1,27 @@
+(TeX-add-style-hook "colmexStrategy2015"
+ (lambda ()
+    (LaTeX-add-labels
+     "fr:MxMapProj")
+    (TeX-run-style-hooks
+     "multirow"
+     "tabulary"
+     "tikz"
+     "color"
+     "graphicx"
+     "amsmath"
+     "babel"
+     "es-nodecimaldot"
+     "spanish"
+     "inputenc"
+     "ansinew"
+     "fontenc"
+     "T1"
+     "ae"
+     "url"
+     "beamerthemesplit"
+     ""
+     "latex2e"
+     "beamer10"
+     "beamer"
+     "xcolor=dvipsnames")))
+
