de26d2402307d8bb8881e501c55f8757fb17e601
trelles33 <trelles33@gmail.com>
2020-02-07 09:16:52 -0500

    catching up

--- StrategyPaperAnalysis/integrateMxdistritos.R
+++ StrategyPaperAnalysis/integrateMxdistritos.R
@@ -1,6 +1,9 @@
 #
 # Integrate @Emagar seccion database and processed map (block-equivalency file)
 # Into proposal database
+#
+# Input: proposals.df , mxDistritosData folder
+# output: propfull.df
 
 require(readr)
 require(magrittr)
@@ -78,8 +81,6 @@ extractPlans<-function(.data){
   
 } 
 
-proposals.df <- read_csv("proposals.csv")
-
 
 mapfiles <- grep("csv",dir("mxDistritos-data/sec2013map/",full.names=TRUE),value = TRUE)
 tmp2013.df <- bind_rows(lapply(mapfiles,function(x)extractPlans(read_csv(x)))) %>% mutate(year=2013)
@@ -87,41 +88,4 @@ mapfiles <- grep("csv",dir("mxDistritos-data/sec2017map/",full.names=TRUE),value
 tmp2017.df <- bind_rows(lapply(mapfiles,function(x)extractPlans(read_csv(x)))) %>% mutate(year=2017)
 proplans.df<- bind_rows(tmp2013.df,tmp2017.df) %>% select(-proposer,-tla)
 propfull.df <- full_join(proposals.df,proplans.df)
-save(propfull.df,file="proposals.RData")
-
-# Example of creating plan scores
-
-#secVhat.df <- read_csv("mxDistritos-data/dipfed-seccion-vhat-2015.csv")
-#secVhat.df %<>% mutate(vhat.tot=vhat.left+vhat.pri+vhat.pan)
-secRaw.df <- read_csv("mxDistritos-data/dipfed-seccion-vraw-2015.csv")
-#standardize variable names so parties match proposals
-names(secRaw.df)[!names(secRaw.df) %in% c("edon","seccion","disn","efec")]%<>% toupper()
-
-proposalEfecScore <- function(plan,year,edon,seccion.df) {
-  # Computes maximum deviation from equal voting population in district
-  
-  # merge plan with appropriate seccion data
-  if(is.null(plan)) {
-    return (NA)
-  }
-  plan %<>% mutate(edon=edon)
-  plan %<>% left_join(seccion.df)
-  
-  # score districts
-  vplan <- plan %>% filter(!is.na(district) & district>0)
-  vplan %<>% group_by(district)
-  
-  # select valid district ids
-  dscores<-summarise(vplan,efec = sum(efec,na.rm=TRUE))
-
-  #create plan scores
-  pscores <- max(abs(dscores[[2]]-mean(dscores[[2]])))/mean(dscores[[2]])
-  return (pscores)
-}
-             
-# now we can use  plan scoring functions to add measure to proposals database
-
-#tmp.df <-propfull.df[1:10,]
-#tmp.df %<>% rowwise() %>% mutate(score.efec=proposalEfecScore(plan,year,edon,secRaw.df))
-
-rm(proplans.df, proposals.df,secRaw.df,tmp2013.df,tmp2017.df,mapfiles,extractPlans)
+#save(propfull.df,file="proposals.RData")
