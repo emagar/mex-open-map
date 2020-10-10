cefb52c5a1f3433736f6d2ab0626b8038edaffef
maltman <micah.altman@gmail.com>
2020-10-10 11:26:48 -0400

    Refactor data prep into its own file so it can be called separately by the transparency analysis.

---
+++ StrategyPaperAnalysis/PrepData.R
@@ -0,0 +1,33 @@
+### PrepData
+###
+### Ingest and structure the core data for both Strategy, Transparency, and Algorithmic Analyses
+###
+
+## Libraries 
+require(tidyverse)
+require(magrittr)
+require(tidylog)
+
+## Party Type Codes
+
+#Major - PRD, PAN, PRI / Minor - PT, PVEM, ES, MORENA, MC, PNA / Admin - CLV, Algorithm, Junta, INE, derfe / Other - PRD51
+parties.df <- structure(list(actor = structure(c(2L, 7L, 9L, 10L, 11L, 13L, 
+                                                 14L, 15L, 1L, 6L, 12L, 5L, 4L, 8L, 3L), .Label = c("Algorithm", 
+                                                                                                    "CLV", "derfe", "ES", "INE", "Junta", "MC", "MORENA", "PAN", 
+                                                                                                    "PNA", "PRD", "PRD51", "PRI", "PT", "PVEM"), class = "factor"), 
+                             partytype = structure(c(1L, 3L, 2L, 3L, 2L, 2L, 3L, 3L, 1L, 
+                                                     1L, 4L, 1L, 3L, 3L, 1L), .Label = c("admin", "major", "minor", 
+                                                                                         "other"), class = "factor")), class = "data.frame", row.names = c(NA, 
+                                                                                                                                                           -15L))                                                                                                                                                     
+
+## Proposal events database: proposal.df
+source("Normalize.R") # reads in Pre-Normalized IFE tables 
+## Ruling party by year and state: grule.df , controlByWindow() helper function
+source("RulingParty_micah.R") # Bring in ruling party db
+## Integrate proposed plans: profull.df
+source("integrateMxdistritos.R")
+rm(proposals.df)
+propfull.df %<>% left_join(parties.df)
+
+
+
